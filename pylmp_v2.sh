b000b856098a5144c8d6bfbf44b263acdfd4e487
MikhailKuklin <mkuklin@puhti-login1.bullx>
2020-09-18 10:37:39 +0300

    Adding missing addmasses part

--- pylmp_v2.sh
+++ pylmp_v2.sh
@@ -11,6 +11,30 @@ echo "from ase.io import read,write; atoms=read(\"new.xyz\"); atoms.center(vacuu
 cp new.lammps-data tmp1
 
 #tmp solution
+{
+if grep -q -w "C" tmp.xyz && grep -q -w "H" tmp.xyz && grep -q -w "O" tmp.xyz;
+then
+        sed '/Atoms/iMasses\n\n3   15.99900000    # O\n2   1.00800000    # H\n1   12.01100000    # C\n' tmp1 > tmp2;
+elif grep -q -w "C" tmp.xyz && grep -q -w "H" tmp.xyz;
+then
+        sed '/Atoms/iMasses\n\n2   1.00800000    # H\n1   12.01100000    # C\n' tmp1 > tmp2;
+elif grep -q -w "C" tmp.xyz && grep -q -w "O" tmp.xyz;
+then
+        sed '/Atoms/iMasses\n\n2   15.99900000    # O\n1   12.01100000    # C\n' tmp1 > tmp2;
+elif grep -q -w "H" tmp.xyz && grep -q -w "O" tmp.xyz;
+then
+        sed '/Atoms/iMasses\n\n2   15.99900000    # O\n1   1.00800000    # H' tmp1 > tmp2;
+elif grep -q -w "H" tmp.xyz tmp.xyz;
+then
+        sed '/Atoms/iMasses\n\n1   1.00800000    # H' tmp1 > tmp2;
+elif grep -q -w "C" tmp.xyz tmp.xyz;
+then
+        sed '/Atoms/iMasses\n\n1   12.01100000    # C\n' tmp1 > tmp2;
+else
+        sed '/Atoms/iMasses\n\n1   15.99900000    # O' tmp1 > tmp2;
+fi
+}
+
 sh addmasses.sh
 
 # here and below transformation for adding charges column to data file as required by LAMPPS for ReaxFF
