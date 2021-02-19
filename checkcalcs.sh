70c55a7b11aeeee80479eb5146cb62a636f01ed2
Mikhail Kuklin <57541345+MikhailKuklin@users.noreply.github.com>
2021-02-19 09:30:04 +0200

    Update checkcalcs.sh

--- checkcalcs.sh
+++ checkcalcs.sh
@@ -2,8 +2,9 @@
 
 squeue -u mkuklin | awk '{print $1}' |  cut -d "_" -f1 | uniq | tail -n +2 > tmpl
 
-echo -e "\033[37mSTATUS\033[0m" " |" "\033[37mTime\033[0m" "    |" "\033[37mID\033[0m" "     |" "\033[37mLOCATION\033[0m" 
-echo "-----------------------------------------------------------------------------------------------------------------"
+echo ""
+echo -e "\033[37mSTATUS\033[0m" "     |" "\033[37mRUN TIME\033[0m" "|" "\033[37mID\033[0m" "     |" "\033$
+echo "---------------------------------------------------------------------------------------------------$
 
 for i in $(cat tmpl)
 do
@@ -11,10 +12,11 @@ time=$(scontrol show job $i | grep "RunTime=" | head -n 1 | awk '{print $1}' | c
 runtime=$(scontrol show job $i | grep "RunTime=" | awk '{print $1}' | cut -c 9-)
 if [ "$time" == "00:00:00" ]
 then
-echo -e "\033[31mNot started\033[0m" "| "00:00:00" |" $i "|" $(scontrol show job $i | grep WorkDir | cut -c 12-)
+echo -e "\033[31mNot started\033[0m" "| "00:00:00" |" $i "|" $(scontrol show job $i | grep WorkDir | cut $
 else
-echo -e "\033[32mRunning\033[0m" "|" $runtime" |" $i "|" $(scontrol show job $i | grep WorkDir | cut -c 12- | uniq)
+echo -e "\033[32mRunning\033[0m" "    |" $runtime" |" $i "|" $(scontrol show job $i | grep WorkDir | cut $
 fi
 done
+echo ""
 
-rm tmpl	
+rm tmpl
