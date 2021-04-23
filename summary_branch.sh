fbcb8971cc5ec4ddc8e7ba46689e2039ee298420
Mikhail Kuklin <57541345+MikhailKuklin@users.noreply.github.com>
2021-04-23 09:50:09 +0300

    Update summary_branch.sh
    default sigmas are taken from template folders

--- summary_branch.sh
+++ summary_branch.sh
@@ -9,7 +9,7 @@ cd $1
 # create list of existing folders and extract T from their input files
 
 cd turbogap/t01
-ls -1v step* | grep 'step*' > list
+ls -1v template* | grep 'template*' > list
 sed -i 's/://g' list
 for i in $(cat list); do cd $i; grep "t_beg" input; cd ../; done > tbeg
 awk '{print $3}' tbeg > tbeg2
@@ -19,14 +19,24 @@ mv tbeg2 ../../training/t01
 # create list of existing folders and extract default sigma and number of structures in the database
 
 cd ../../
-cd training/t01
-ls -1v vasp step* | grep 'step*' > list
-sed -i 's/://g' list
-sed -i '1 i\vasp' list
-for i in $(cat list); do cd $i; grep "default_sigma" training_forces.sh; cd ../; done > defsigma
-for i in $(cat list); do cd $i; grep "Lattice" new.xyz | wc -l; cd ../; done > strnum
+cd training/
+ls -1v template* | grep 'template*' > list2
+sed -i 's/://g' list2
+sed -i '1 i\vasp' list2
+head -n -1 list2 > list_ds
+rm list2
+for i in $(cat list_ds); do cd $i; grep "default_sigma" training_forces.sh; cd ../; done > defsigma
 awk '{print $1}' defsigma > defsigma2
 sed -i 's/default_sigma={//' defsigma2
+rm defsigma
+mv defsigma2 ./t01
+cd t01
+ls -1v step* | grep 'step*' > list2
+sed -i 's/://g' list2
+sed -i '1 i\vasp' list2
+head -n -1 list2 > list
+rm list2
+for i in $(cat list); do cd $i; grep "Lattice" new.xyz | wc -l; cd ../; done > strnum
 
 # extract rmse per structure and per atom
 
@@ -40,8 +50,8 @@ awk '{print $2}' tmp3 > rmse_qm9_pa
 # print all parameters
 
 echo ''
-echo -e "\033[31mFolder\033[0m" "\033[31mT(K)\033[0m " "  \033[31mReg(eV)\033[0m" " \033[31mData\033[0m" " \033[31mRMSE per str\033[0m" "       \033[31mRMSE per atom\033[0m"
+echo -e "\033[31mFolder\033[0m" " \033[31mT(K)\033[0m " "  \033[31mReg(eV)\033[0m" " \033[31mData\033[0m" " \033[31mRMSE per str\033[0m" "       \033[31mRMSE per atom\033[0m"
 echo "--------------------------------------------------------------------"
 paste list tbeg2 defsigma2 strnum rmse_qm9 rmse_qm9_pa | column -tc6
 echo ''
-rm defsigma* list* strnum
+rm list* strnum
