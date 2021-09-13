7de39978c102e993a71e23fc2795f58e2ffa8f27
Mikhail Kuklin <mikhail@mikhail.lan>
2021-09-13 21:44:56 +0300

    script for extracting unique tags and their energy sigmas for xyz databases

---
+++ extract_unique_tags_es_xyz.py
@@ -0,0 +1,22 @@
+# import module and read xyz file
+
+from ase.io import read, write
+
+file=read('last3.xyz', index=":")
+
+
+# create list of tags
+
+tags = []
+
+for structure in file:
+  if structure.info['config_type'] not in tags:
+    tags.append[structure.info['config_type'])
+
+# extract unique tags and energy sigma
+
+for key in tags:
+  for structure in file:
+    if structure.info['config_type'] == key and structure.info['energy_sigma'] not in dict.get(key):
+      dict[key].append(structure.info['energy_sigma'])
+
